272744808af4527021c37d667f286279f97582fd
KouSiqi <113419191+karrykkk@users.noreply.github.com>
2024-02-24 11:54:18 +0800

    Update dpm.sh

--- ddpm_and_guided/dpm.sh
+++ ddpm_and_guided/dpm.sh
@@ -1,22 +1,21 @@
-# dpm_solverUQ.py's imagenet128 skip FID
+# imagenet128
 DEVICES="7"
 data="imagenet128_guided"
 steps="50"
 mc_size="10"
-sample_batch_size="12"
-total_n_sample="48"
+sample_batch_size="16"
+total_n_sample="96"
+# the size of dataset used for Laplace approximation: size_of_total_dataset//train_la_data_size
 train_la_data_size="200"
 fixed_class="10"
 DIS="logSNR"
-seed="1235"
-
 
 CUDA_VISIBLE_DEVICES=$DEVICES python dpm_solver_skipUQ.py \
 --config $data".yml" --timesteps=$steps --skip_type=$DIS --train_la_batch_size 16 \
 --mc_size=$mc_size --sample_batch_size=$sample_batch_size --fixed_class=$fixed_class --train_la_data_size=$train_la_data_size \
 --total_n_sample=$total_n_sample --seed=$seed
 
-# dpm_solverUQ.py's CELEBA skip FID
+# CELEBA
 # DEVICES="0"
 # data="celeba"
 # steps="50"
